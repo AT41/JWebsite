5fe4d24fa2c57a459746f27d436514bad3a256e5
anthony.tang2@ucalgary.ca <anthony.tang2@ucalgary.ca>
2020-05-22 20:58:39 -0600

    Prettier: New commits will run prettier automatically

--- src/app/shared/users-lib/user-authentication-service/user-authentication.service.ts
+++ src/app/shared/users-lib/user-authentication-service/user-authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BackendService } from 'src/app/shared/services/backend-service/backend.service';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +8,17 @@ import {Observable, of} from 'rxjs';
 export class UserAuthenticationService {
   private readonly sessionsUrl = `http://localhost:3000/sessions`;
   private readonly localStorageNonceKey = 'currentNonce';
-  private readonly localStorageUsernameKey = 'currentUsername'
+  private readonly localStorageUsernameKey = 'currentUsername';
 
-  constructor(private backendService: BackendService) { }
+  constructor(private backendService: BackendService) {}
 
   public isAuthorized$(): Observable<boolean> {
     const username = window.localStorage.getItem(this.localStorageUsernameKey);
     const session_token = window.localStorage.getItem(this.localStorageNonceKey);
 
-    return this.backendService.httpRequest(`${this.sessionsUrl}?username=${username}&session_token=${session_token}`) as Observable<boolean>;
+    return this.backendService.httpRequest(
+      `${this.sessionsUrl}?username=${username}&session_token=${session_token}`
+    ) as Observable<boolean>;
   }
 
   public saveLoginSession(username: string, sessionNonce: string) {
@@ -25,7 +27,7 @@ export class UserAuthenticationService {
   }
 
   // TODO ANTHONY Make sure the methods calling this all contain the correct Nonces, or make a separate guard for verification
-  public getLoggedInUser(): {username: string, sessionToken: string} {
+  public getLoggedInUser(): { username: string; sessionToken: string } {
     return {
       username: window.localStorage.getItem(this.localStorageUsernameKey),
       sessionToken: window.localStorage.getItem(this.localStorageNonceKey)
